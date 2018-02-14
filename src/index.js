64949b2ba04d0fc883245d0ee1543554fafad7d8
Yvan Yang <yvan.yang@ulsee.com>
2018-02-14 23:11:54 +0800

    生产环境构建

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
-import _ from 'lodash';
+import { cube } from './math.js';
+
+if (process.env.NODE_ENV !== 'production') {
+  console.log('Looks like we are in development mode!');
+}
 
 function component() {
-    var element = document.createElement('div');
-  
-    // Lodash, now imported by this script
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  
-    return element;
-  }
-  
-  document.body.appendChild(component());
+  var element = document.createElement('pre');
+
+  element.innerHTML = ['Hello webpack!', '5 cubed is equal to ' + cube(5)].join(
+    '\n\n'
+  );
+
+  return element;
+}
+
+document.body.appendChild(component());
---
+++ webpack.prod.js
@@ -0,0 +1,21 @@
+const webpack = require('webpack');
+const merge = require('webpack-merge');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const common = require('./webpack.common.js');
+
+module.exports = merge(common, {
+  devtool: 'source-map', // 避免在生产中使用 inline-*** 和 eval-***，因为它们可以增加 bundle 大小，并降低整体性能。
+  plugins: [
+    new UglifyJSPlugin({
+      sourceMap: true
+    }),
+    new webpack.DefinePlugin({
+      // 1. 技术上讲，NODE_ENV 是一个由 Node.js 暴露给执行脚本的系统环境变量。
+      // 通常用于决定在开发环境与生产环境(dev-vs-prod)下，服务器工具、构建脚本和客户端 library 的行为。
+      // 然而，与预期不同的是，无法在构建脚本 webpack.config.js 中，将 process.env.NODE_ENV 设置为 "production"，请查看 #2537。
+      // 因此，例如 process.env.NODE_ENV === 'production' ? '[name].[hash].bundle.js' : '[name].bundle.js' 这样的条件语句，在 webpack 配置文件中，无法按照预期运行。
+      // 2. 还要注意，任何位于 /src 的本地代码都可以关联到 process.env.NODE_ENV 环境变量，所以以下检查也是有效的：
+      'process.env.NODE_ENV': JSON.stringify('production')
+    })
+  ]
+});
